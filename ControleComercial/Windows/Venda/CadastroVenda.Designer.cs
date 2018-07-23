27468d769dcfa74b8cf9d339fc5cbf26ae4d582d
patrikmuller <patrik.muller@hotmail.com>
2018-07-23 16:12:35 -0300

    Cadastro de Venda

--- ControleComercial/Windows/Venda/CadastroVenda.Designer.cs
+++ ControleComercial/Windows/Venda/CadastroVenda.Designer.cs
@@ -47,15 +47,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.GridProdutos = new System.Windows.Forms.DataGridView();
             this.Cabecalho.SuspendLayout();
             this.Rodape.SuspendLayout();
             this.Corpo.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridProdutos)).BeginInit();
             this.SuspendLayout();
             // 
             // Cabecalho
@@ -237,14 +240,6 @@
             this.panel1.Size = new System.Drawing.Size(350, 378);
             this.panel1.TabIndex = 1;
             // 
-            // panel3
-            // 
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(0, 40);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(350, 338);
-            this.panel3.TabIndex = 2;
-            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.label1);
@@ -263,6 +258,24 @@
             this.label1.Size = new System.Drawing.Size(152, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Lista de Produtos";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.GridProdutos);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 40);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(350, 338);
+            this.panel3.TabIndex = 2;
+            // 
+            // GridProdutos
+            // 
+            this.GridProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridProdutos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridProdutos.Location = new System.Drawing.Point(0, 0);
+            this.GridProdutos.Name = "GridProdutos";
+            this.GridProdutos.Size = new System.Drawing.Size(350, 338);
+            this.GridProdutos.TabIndex = 0;
             // 
             // CadastroVenda
             // 
@@ -286,6 +299,8 @@
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GridProdutos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -297,7 +312,6 @@
         private System.Windows.Forms.Panel Corpo;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel4;
@@ -314,5 +328,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtIdCarrinho;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView GridProdutos;
     }
 }
