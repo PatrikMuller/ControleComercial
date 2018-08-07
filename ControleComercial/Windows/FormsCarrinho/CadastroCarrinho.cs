a521a8ed8bbb32b42b874f09fade7d44c5bfa05b
patrikmuller <patrik.muller@hotmail.com>
2018-08-06 21:14:19 -0300

    Commit CarrinhoItem

--- ControleComercial/Windows/FormsCarrinho/CadastroCarrinho.cs
+++ ControleComercial/Windows/FormsCarrinho/CadastroCarrinho.cs
@@ -16,70 +16,73 @@ namespace Windows.FormsCarrinho
     public partial class CadastroCarrinho : Form
     {
 
+        //Modelo
+        Carrinho carrinho = new Carrinho();
         CarrinhoItem carrinhoItem = new CarrinhoItem();
+
+        //Persistencia
+        CarrinhoAccess CarrinhoDao = new CarrinhoAccess();       
         CarrinhoItemAccess carrinhoItemDao = new CarrinhoItemAccess();
 
 
-        private void tabelaGridProduto(DataGridView g, IList<CarrinhoItem> l)
+        private DataTable dadosGridProduto(IList<CarrinhoItem> l)
         {
             DataTable dt = new DataTable();
 
-            dt.Columns.Add("id", System.Type.GetType("System.String"));
+            //dt.Columns.Add("id", System.Type.GetType("System.String"));
             dt.Columns.Add("produto", System.Type.GetType("System.String"));
-            dt.Columns.Add("qtd", System.Type.GetType("System.String"));
-            dt.Columns.Add("preco", System.Type.GetType("System.String"));
+            dt.Columns.Add("qtd", System.Type.GetType("System.Decimal"));
+            dt.Columns.Add("preco", System.Type.GetType("System.Decimal"));
+            dt.Columns.Add("desconto", System.Type.GetType("System.Decimal"));
                         
-            //for (int i = 1; i < 10; i++)
             foreach (var carrinhoItem in l)
             {
-                dt.Rows.Add(new String[] { Convert.ToString(carrinhoItem.Id), carrinhoItem.Item.Nome, Convert.ToString(carrinhoItem.Quantidade), Convert.ToString(carrinhoItem.Preco) });
+                dt.Rows.Add(new String[] { carrinhoItem.Item.Nome, Convert.ToString(carrinhoItem.Quantidade), Convert.ToString(carrinhoItem.Preco), Convert.ToString(carrinhoItem.Desconto) });
             }
 
-            //return dt;
-
-            g.DataSource = null;
-            g.DataSource = dt;
+            return dt;            
         }
 
+        private void configuraGridProdutos()
+        {
+            
+            //GridProdutos.Columns[1].Width = 200;
+            GridProdutos.Columns[1].DefaultCellStyle.Format = "###,###,###,##0.000";
+            GridProdutos.Columns[2].DefaultCellStyle.Format = "R$ ###,###,###,##0.00";
+            GridProdutos.Columns[3].DefaultCellStyle.Format = "R$ ###,###,###,##0.00";
+
+        }
 
 
         public CadastroCarrinho()
         {
+
             InitializeComponent();
 
-            //tabelaGridProduto(GridProdutos, carrinhoItemDao.Lista(Convert.ToInt32(txtIdCarrinho.Text)));
-            tabelaGridProduto(GridProdutos, carrinhoItemDao.Lista(31));
+            GridProdutos.DataSource = dadosGridProduto(carrinhoItemDao.Lista(Convert.ToInt32(txtIdCarrinho.Text)));
+            //GridProdutos.DataSource = dadosGridProduto(carrinhoItemDao.Lista(31));
+            configuraGridProdutos();
 
         }
         
         private void btnNovoCarrinho_Click(object sender, EventArgs e)
         {
-            Carrinho carrinho = new Carrinho();
-            CarrinhoAccess CarrinhoDao = new CarrinhoAccess();
 
-
-            //carrinho.Id = 15;
             carrinho.Id = Convert.ToInt32(txtIdCarrinho.Text);
             carrinho.Data = DateTime.Now;
-
-            //txtIdCarrinho.Text = Convert.ToString(carrinho.Id);
             txtIdCarrinho.Text = Convert.ToString(CarrinhoDao.Novo(carrinho));
             txtDataCarrinho.Text = Convert.ToString(carrinho.Data);
+
         }
 
         private void btnGravar_Click(object sender, EventArgs e)
-        {
-            Carrinho carrinho = new Carrinho();
-            CarrinhoAccess CarrinhoDao = new CarrinhoAccess();
-
-
-            //carrinho.Id = 15;
+        {            
+            
             carrinho.Id = Convert.ToInt32(txtIdCarrinho.Text);
             carrinho.Data = DateTime.Now;
-
-            //txtIdCarrinho.Text = Convert.ToString(carrinho.Id);
             txtIdCarrinho.Text = Convert.ToString(CarrinhoDao.Gravar(carrinho));
             txtDataCarrinho.Text = Convert.ToString(carrinho.Data);
+
         }
 
         private void btnLocalizar_Click(object sender, EventArgs e)
@@ -88,10 +91,10 @@ namespace Windows.FormsCarrinho
             
             if (form.ShowDialog() == DialogResult.OK)
             {
+
                 DataGridViewRow Row = new DataGridViewRow();
                 Row = form.Row();
                 
-                Carrinho carrinho = new Carrinho();
                 carrinho.Id = Convert.ToInt32(txtIdCarrinho.Text);
                 Item item = new Item();
                 item.Id = Convert.ToInt32(Row.Cells[0].Value);
@@ -99,19 +102,14 @@ namespace Windows.FormsCarrinho
                 carrinhoItem.Carrinho = carrinho;
                 carrinhoItem.Item = item;
                 carrinhoItem.Quantidade = Convert.ToDecimal(txtQuantidade.Text);
-                carrinhoItem.Preco = Convert.ToDecimal(15.78);
+                carrinhoItem.Preco = Convert.ToDecimal(Row.Cells[3].Value);
                 carrinhoItem.Desconto = Convert.ToDecimal(0.00);
                 carrinhoItemDao.Novo(carrinhoItem);
-                tabelaGridProduto(GridProdutos, carrinhoItemDao.Lista(31));
+                GridProdutos.DataSource = dadosGridProduto(carrinhoItemDao.Lista(Convert.ToInt32(txtIdCarrinho.Text)));
+                configuraGridProdutos();
 
-                //GridProdutos.DataSource = null;
-                //GridProdutos.DataSource = carrinhoItemDao.Lista();
-
-                //GravarNotaItem(Convert.ToInt32(Row.Cells[0].Value), "patrikmuller");
-                //setarGridItem();
             }
 
-            //ConfiguraCampos();
         }
 
 
--- ControleComercial/Windows/FormsCarrinho/LocalizarItem.cs
+++ ControleComercial/Windows/FormsCarrinho/LocalizarItem.cs
@@ -16,59 +16,61 @@ namespace Windows.FormsCarrinho
     public partial class LocalizarItem : Form
     {
 
-        //private Int32 idcarrinho;
-        //private Decimal quantidade;
-        //private CadastroCarrinho CadastroCarrinho;
+        //Model
         Item item = new Item();
+
+        //
         ItemAccess itemAccess = new ItemAccess();
 
 
-        //public LocalizarItem(Int32 idcarrinho, Decimal quantidade, CadastroCarrinho form)
-        public LocalizarItem()
+        private DataTable dadosGrid(IList<Item> l)
         {
-            //this.idcarrinho = idcarrinho;
-            //this.quantidade = quantidade;
-            //this.CadastroCarrinho = form;
-            InitializeComponent();
-        }
 
-        private void tabelaGrid(DataGridView g, IList<Item> l)
-        {
             DataTable dt = new DataTable();
 
             dt.Columns.Add("id", System.Type.GetType("System.String"));
             dt.Columns.Add("produto", System.Type.GetType("System.String"));
-            dt.Columns.Add("qtd", System.Type.GetType("System.String"));
-
-            //for (int i = 1; i < 10; i++)
+            dt.Columns.Add("qtd", System.Type.GetType("System.Decimal"));
+            dt.Columns.Add("preço", System.Type.GetType("System.Decimal"));
+            
             foreach (var item in l)
             {
-                dt.Rows.Add(new String[] { Convert.ToString(item.Id), item.Nome, Convert.ToString(item.Quantidade) });
+                dt.Rows.Add(new String[] { Convert.ToString(item.Id), item.Nome, Convert.ToString(item.Quantidade), Convert.ToString(item.Preco) });
             }
 
-            //return dt;
+            return dt;
 
-            g.DataSource = null;
-            g.DataSource = dt;
+        }
+
+        private void configuraGrid()
+        {
+
+            grid.Columns[1].Width = 200;
+            grid.Columns[2].DefaultCellStyle.Format = "###,###,###,##0.000";
+            grid.Columns[3].DefaultCellStyle.Format = "R$ ###,###,###,##0.00";
+
         }
 
         public DataGridViewRow Row()
         {
+
             return grid.CurrentRow;
+
         }
+        
+        public LocalizarItem()
+        {
 
+            InitializeComponent();
 
+        }
+                
         private void ListaItem_Activated(object sender, EventArgs e)
         {
-            
 
-            //grid.DataSource = null;
-            //grid.DataSource = itemAccess.Lista();
+            grid.DataSource = dadosGrid(itemAccess.Lista());
+            configuraGrid();
 
-            tabelaGrid(grid, itemAccess.Lista());
-
-            //Grid.DataSource = null;
-            //Grid.DataSource = lista;
         }
 
         private void btnInserir_Click(object sender, EventArgs e)
@@ -78,29 +80,6 @@ namespace Windows.FormsCarrinho
             {
                 this.DialogResult = DialogResult.OK;
             }
-
-
-            //CarrinhoItem carrinhoItem = new CarrinhoItem();
-            //CarrinhoItemAccess CarrinhoItemDao = new CarrinhoItemAccess();
-
-            ////carrinho.Id = 15;
-            //carrinhoItem.Carrinho.Id = idcarrinho;
-            //carrinhoItem.Item = grid.ro
-
-
-            ////txtIdCarrinho.Text = Convert.ToString(carrinho.Id);
-            //txtIdCarrinho.Text = Convert.ToString(CarrinhoDao.Gravar(carrinhoItem));
-            //txtDataCarrinho.Text = Convert.ToString(carrinhoItem.Data);
-
-            //CadastroItem form = new CadastroItem();
-            //form.ShowDialog();
-
-            //Conta novaConta = new ContaCorrente();
-            //novaConta.Titular = new Cliente(txtTitular.Text);
-            //novaConta.Numero = Convert.ToInt32(txtNumero.Text);
-
-            //formPrincipal.AdicionaConta(novaConta);
-            //Close();
 
         }
 
